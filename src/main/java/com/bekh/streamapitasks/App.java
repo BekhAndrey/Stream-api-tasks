9a6b46bcad5200c8ddf44718672b3058ada16436
andrei.bekh <andrei.bekh@leverx.com>
2021-12-23 12:41:47 +0300

    Initial commit

---
+++ src/main/java/com/bekh/streamapitasks/App.java
@@ -0,0 +1,19 @@
+package com.bekh.streamapitasks;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+    }
+}
